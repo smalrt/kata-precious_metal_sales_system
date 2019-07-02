dca96b7cc0c00d43b555c6152325cfb9a26ae9c4
smalrt <huxiaohua@csii.com.cn>
2019-07-02 23:01:06 +0800

    add by huxiaohua 重构计算优惠券代码

--- src/main/java/com/coding/sales/OrderApp.java
+++ src/main/java/com/coding/sales/OrderApp.java
@@ -83,57 +83,7 @@ public class OrderApp {
             OrderItemRepresentation orderItemRepresentation = new OrderItemRepresentation(productId, product.getProductName(), price, productCount, singleProductTotalAmt);
             orderItems.add(orderItemRepresentation);
 
-
             //处理优惠信息
-            /*BigDecimal discountAmt = new BigDecimal(0);
-            BigDecimal discountAmt_card = new BigDecimal(0);
-            //客户优惠券
-            List<String> discoutCards = viper.getDiscountList();
-            String productDiscountId = product.getDiscountId();
-            if (productDiscountId != null && !"".equals(productDiscountId)) {
-                if (discoutCards != null && discoutCards.size() > 0) {
-                    for (String discountId : discoutCards) {
-                        if (discountId.equals(productDiscountId)) {
-                            //优惠券
-                            Discount discount = DiscountData.discountMap.get(productDiscountId);
-                            //使用优惠券计算优惠金额
-                            discountAmt_card = singleProductTotalAmt.multiply(new BigDecimal(1).subtract(discount.getValue()));
-                            break;
-                        }
-                    }
-                }
-            }
-
-            //是否满足满减规则
-            BigDecimal discountAmt_manjian = new BigDecimal(0);
-            List<String> rule_full_reductionList = product.getRule_full_reduction();
-            if (rule_full_reductionList != null && rule_full_reductionList.size() > 0) {
-                for (String rule_full_reduction : rule_full_reductionList) {
-                    //type|金额/件数|减金额/优惠件数
-                    String[] rules = rule_full_reduction.split("\\|");
-                    if ("0".equals(rules[0])) {//满减
-                        if (singleProductTotalAmt.compareTo(new BigDecimal(rules[1])) >= 0) {
-                            discountAmt_manjian = discountAmt_manjian.compareTo(new BigDecimal(rules[2])) > 0 ? discountAmt_manjian : new BigDecimal(rules[2]);
-                        }
-                    } else if ("1".equals(rules[0])) {//按件数优惠
-                        if (Integer.parseInt(productCount.toString()) >= Integer.parseInt(rules[1])) {
-                            discountAmt_manjian = price.multiply(new BigDecimal(rules[2]));
-                        }
-                    }
-                }
-
-            }
-
-            //最优惠价格
-            discountAmt = discountAmt_card.compareTo(discountAmt_manjian) > 0 ? discountAmt_card : discountAmt_manjian;
-
-            //总优惠价格
-            discountTotalAmt = discountTotalAmt.add(discountAmt);
-
-            if (discountAmt.compareTo(new BigDecimal(0)) > 0) {
-                DiscountItemRepresentation discountItemRepresentation = new DiscountItemRepresentation(productId, product.getProductName(), discountAmt);
-                discountItems.add(discountItemRepresentation);
-            }*/
             discountTotalAmt = discountTotalAmt.add(DiscountHandler.handler(discountItems, viper, product, productCount));
 
         }
@@ -143,27 +93,9 @@ public class OrderApp {
         List<PaymentRepresentation> paymentItems = new ArrayList<PaymentRepresentation>();
         List<PaymentCommand> paymentCommandList = command.getPayments();
         PayHandler.handler(paymentItems, paymentCommandList);
-        /*for (PaymentCommand paymentCommand : paymentCommandList) {
-            PaymentRepresentation paymentRepresentation = new PaymentRepresentation(paymentCommand.getType(), paymentCommand.getAmount());
-            paymentItems.add(paymentRepresentation);
-        }*/
 
         //计算新增积分
         int addScore = VipLevelHandler.handlerPoint(viper, totalAmt.subtract(discountTotalAmt));
-        /*int addScore = 0;
-        BigDecimal totalAmt_jsscore = totalAmt.subtract(discountTotalAmt);
-        VipLevelEnum vipLevel = viper.getLevel();
-        if (VipLevelEnum.NORMAL.equals(vipLevel)) {
-            addScore += Integer.valueOf(totalAmt_jsscore.setScale(0, BigDecimal.ROUND_HALF_UP).toString());
-        } else if (VipLevelEnum.GOLD.equals(vipLevel)) {
-            String totalScore = totalAmt_jsscore.multiply(new BigDecimal(1.5)).toString();
-            addScore += Integer.parseInt(String.valueOf(Math.ceil(Double.parseDouble(totalScore))));
-        } else if (VipLevelEnum.PLATINUM.equals(vipLevel)) {
-            String totalScore = totalAmt_jsscore.multiply(new BigDecimal(1.8)).toString();
-            addScore += Integer.parseInt(String.valueOf(Math.ceil(Double.parseDouble(totalScore))));
-        } else {
-            addScore += Integer.parseInt(totalAmt_jsscore.toString()) * 2;
-        }*/
 
         //新的积分
         int newScore = viper.getScore() + addScore;
--- src/main/java/com/coding/sales/discount/DiscountHandler.java
+++ src/main/java/com/coding/sales/discount/DiscountHandler.java
@@ -15,9 +15,10 @@ public class DiscountHandler {
         BigDecimal singleProductTotalAmt = price.multiply(productCount);
         //处理优惠信息
         BigDecimal discountAmt = new BigDecimal(0);
-        BigDecimal discountAmt_card = new BigDecimal(0);
-        //客户优惠券
-        List<String> discoutCards = viper.getDiscountList();
+//        BigDecimal discountAmt_card = new BigDecimal(0);
+        //客户优惠券优惠金额
+        BigDecimal discountAmt_card = cal_discountcard(viper,product,productCount);
+        /*List<String> discoutCards = viper.getDiscountList();
         String productDiscountId = product.getDiscountId();
         if (productDiscountId != null && !"".equals(productDiscountId)) {
             if (discoutCards != null && discoutCards.size() > 0) {
@@ -31,10 +32,10 @@ public class DiscountHandler {
                     }
                 }
             }
-        }
+        }*/
 
         //是否满足满减规则
-        BigDecimal discountAmt_manjian = new BigDecimal(0);
+        /*BigDecimal discountAmt_manjian = new BigDecimal(0);
         List<String> rule_full_reductionList = product.getRule_full_reduction();
         if (rule_full_reductionList != null && rule_full_reductionList.size() > 0) {
             for (String rule_full_reduction : rule_full_reductionList) {
@@ -51,7 +52,8 @@ public class DiscountHandler {
                 }
             }
 
-        }
+        }*/
+        BigDecimal discountAmt_manjian = cal_discount_manjian(product, productCount);
 
         //最优惠价格
         discountAmt = discountAmt_card.compareTo(discountAmt_manjian) > 0 ? discountAmt_card : discountAmt_manjian;
@@ -61,5 +63,56 @@ public class DiscountHandler {
             discountItems.add(discountItemRepresentation);
         }
         return discountAmt;
+    }
+
+    /**
+     * 计算优惠券优惠金额
+     * @param viper
+     * @param product
+     * @param productCount
+     * @return
+     */
+    private static BigDecimal cal_discountcard(Vip viper, Product product, BigDecimal productCount){
+        BigDecimal discountAmt_card = new BigDecimal(0);
+        //单个商品总金额
+        BigDecimal singleProductTotalAmt = product.getPrice().multiply(productCount);
+        List<String> discoutCards = viper.getDiscountList();
+        String productDiscountId = product.getDiscountId();
+        if (productDiscountId != null && !"".equals(productDiscountId)) {
+            if (discoutCards != null && discoutCards.size() > 0) {
+                for (String discountId : discoutCards) {
+                    if (discountId.equals(productDiscountId)) {
+                        //优惠券
+                        Discount discount = DiscountData.discountMap.get(productDiscountId);
+                        //使用优惠券计算优惠金额
+                        discountAmt_card = singleProductTotalAmt.multiply(new BigDecimal(1).subtract(discount.getValue()));
+                        break;
+                    }
+                }
+            }
+        }
+        return discountAmt_card;
+    }
+
+    private static BigDecimal cal_discount_manjian(Product product, BigDecimal productCount){
+        BigDecimal singleProductTotalAmt = product.getPrice().multiply(productCount);
+        BigDecimal discountAmt_manjian = new BigDecimal(0);
+        List<String> rule_full_reductionList = product.getRule_full_reduction();
+        if (rule_full_reductionList != null && rule_full_reductionList.size() > 0) {
+            for (String rule_full_reduction : rule_full_reductionList) {
+                //type|金额/件数|减金额/优惠件数
+                String[] rules = rule_full_reduction.split("\\|");
+                if ("0".equals(rules[0])) {//满减
+                    if (singleProductTotalAmt.compareTo(new BigDecimal(rules[1])) >= 0) {
+                        discountAmt_manjian = discountAmt_manjian.compareTo(new BigDecimal(rules[2])) > 0 ? discountAmt_manjian : new BigDecimal(rules[2]);
+                    }
+                } else if ("1".equals(rules[0])) {//按件数优惠
+                    if (Integer.parseInt(productCount.toString()) >= Integer.parseInt(rules[1])) {
+                        discountAmt_manjian = product.getPrice().multiply(new BigDecimal(rules[2]));
+                    }
+                }
+            }
+        }
+        return discountAmt_manjian;
     }
 }
